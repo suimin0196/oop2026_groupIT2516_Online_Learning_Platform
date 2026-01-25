206a534202bc857f7e59680f4e33a26db11a6af3
Nurislam <254143@astanait.edu.kz>
2026-01-25 21:41:40 +0500

    Ospanali Nurislam - add enrollment repository

---
+++ src/main/java/com/company/repositories/EnrollmentRepository.java
@@ -0,0 +1,13 @@
+package com.company.repositories;
+
+import com.company.models.Course;
+import java.util.List;
+
+public interface EnrollmentRepository {
+
+    // Enroll user to course
+    void enroll(int userId, int courseId);
+
+    // Get all courses a user is enrolled in
+    List<Course> findCoursesByUserId(int userId);
+}
