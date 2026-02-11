071c80d85d38dff4261e9dade9dc11ba745c0627
suimin0196 <sui.min0196@gmail.com>
2026-02-11 11:55:19 +0500

    Adil Dyussenov - Add Prepared Statement Setter

---
+++ src/main/java/com/company/db/PreparedStatementSetter.java
@@ -0,0 +1,9 @@
+package com.company.db;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface PreparedStatementSetter {
+    void setValues(PreparedStatement ps) throws SQLException;
+}
