1c429d8a6d94bffff5a8e4e7067ce09be2bc043c
suimin0196 <sui.min0196@gmail.com>
2026-02-11 12:02:52 +0500

    Ospanali Nurislam - Update User model

--- src/main/java/com/company/models/User.java
+++ src/main/java/com/company/models/User.java
@@ -3,7 +3,39 @@ package com.company.models;
 
 // Represents a platform user
 public class User {
-    public int id;
-    public String name;
-    public String email;
+    private int id;
+    private String name;
+    private String email;
+
+    public User() {}
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
